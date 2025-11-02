172c7168b2e1eecd77b5673f99308ddc17ea0885
Mockelyn Maxime <maximemockelyn8@gmail.com>
2025-11-02 17:31:00 +0100

    feat(profile): intégrer la gestion des informations utilisateur et la suppression de compte
    - Ajout d'un formulaire pour mettre à jour les informations utilisateur dans `ProfilGeneralPanel`.
    - Implémentation de l'action de suppression de compte avec confirmation et mise en garde.
    - Mise à jour des styles CSS avec l'import de nouvelles feuilles (`keenicons` et styles généraux).
    - Réorganisation des balises et fichiers CSS dans les vues pour une meilleure gestion des dépendances.
    
    Ces modifications permettent une gestion complète des données utilisateur et améliorent l'interface utilisateur du panneau de profil.

--- app/Livewire/Core/Component/Panel/ProfilGeneralPanel.php
+++ app/Livewire/Core/Component/Panel/ProfilGeneralPanel.php
@@ -2,17 +2,77 @@
 
 namespace App\Livewire\Core\Component\Panel;
 
+use App\Models\User;
+use Filament\Actions\Action;
 use Filament\Actions\Concerns\InteractsWithActions;
 use Filament\Actions\Contracts\HasActions;
+use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Concerns\InteractsWithSchemas;
 use Filament\Schemas\Contracts\HasSchemas;
+use Filament\Schemas\Schema;
+use Filament\Support\Enums\Size;
+use Filament\Support\Icons\Heroicon;
 use Livewire\Component;
 
 class ProfilGeneralPanel extends Component implements HasSchemas, HasActions
 {
     use InteractsWithSchemas, InteractsWithActions;
 
+    public ?array $data = [];
+    public User $user;
 
+    public function mount(): void
+    {
+        $this->user = auth()->user();
+        $this->form->fill($this->user->attributesToArray());
+    }
+
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->label('Nom'),
+
+                TextInput::make('email')
+                    ->label('Email'),
+            ])
+            ->statePath('data')
+            ->model($this->user);
+    }
+
+    public function updateUser(): void
+    {
+        try {
+            $this->user->update($this->data);
+
+            flash()->success('Mise à jour de vos informations réussi')
+                ->setTitle('Mise à jour de vos informations');
+
+
+        }catch (\Exception $e) {
+            \Log::emergency($e->getMessage());
+
+            flash()
+                ->error($e->getMessage())
+                ->setTitle('Erreur Server');
+        }
+    }
+
+    public function deleteUserAction(): Action
+    {
+        return Action::make('delete')
+            ->label('Supprimer mon compte')
+            ->button()
+            ->icon(Heroicon::XMark)
+            ->size(Size::Large)
+            ->extraAttributes([
+                'class' => 'kt-btn kt-btn-destructive text-red-600'
+            ])
+            ->action(function () {
+                $this->user->delete();
+            });
+    }
     public function render(): \Illuminate\Contracts\View\View
     {
         return view('livewire.core.component.panel.profil-general-panel');
