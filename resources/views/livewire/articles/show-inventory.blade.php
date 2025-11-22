362bfb4f66e96be15b9763d61601b06f1ab049b8
Mockelyn Maxime <maximemockelyn8@gmail.com>
2025-11-22 20:43:04 +0100

    feature: Ajout de la vue d'inventaire améliorée
    Cette modification introduit une nouvelle vue d'inventaire avec un affichage détaillé des informations et des actions disponibles.
    
    - Le composant a été renforcé avec des schémas Filament pour l'affichage.
    - Deux actions ont été ajoutées : valider l'inventaire et retour à la liste.
    - La mise en page a été optimisée avec Tailwind CSS.
    
    Cette amélioration permet une meilleure expérience utilisateur et une gestion plus efficace des inventaires.

--- resources/views/livewire/articles/show-inventory.blade.php
+++ resources/views/livewire/articles/show-inventory.blade.php
@@ -1,3 +1,27 @@
-<div>
-    {{-- Stop trying to control. --}}
+<div class="p-4 sm:p-6 lg:p-8">
+    <div class="flex flex-wrap items-center lg:items-end justify-between gap-5">
+        <div class="flex flex-col justify-center gap-2">
+            <h1 class="text-xl font-medium leading-none text-mono">Inventaire</h1>
+            <div class="flex items-center gap-2 text-sm font-normal text-secondary-foreground">
+                #{{ $inventory->code }}
+            </div>
+        </div>
+        <div class="flex items-center gap-2 5">
+            @foreach ($this->getHeaderAction() as $action)
+                {{ $action }}
+            @endforeach
+
+        </div>
+    </div>
+    <div class="kt-separator my-2.5"></div>
+    <div class="kt-container-fluid">
+        <div class="grid lg:grid-cols-4 gap-5 items-stretch">
+            <div class="lg:col-span-1">
+                {{ $this->inventoryInfoList }}
+            </div>
+            <div class="lg:col-span-2">
+            </div>
+        </div>
+    </div>
+
 </div>
